0768eb90aa140e9516cd49e27eecacfd7d0e22d2
Vyacheslav Pogorelskiy <>
2021-10-10 22:29:25 +0300

    Added Injected property wrapper

---
+++ Shared/Core/Injected.swift
@@ -0,0 +1,19 @@
+
+import Foundation
+
+@propertyWrapper struct Injected<Value> {
+    private var storedValue: Value?
+    
+    var wrappedValue: Value? {
+        mutating set {
+            storedValue = newValue
+        }
+        mutating get {
+            guard storedValue == nil else {
+                return storedValue
+            }
+            storedValue = ServiceLocator.sharedInstance.getInstanceOf(type: Value.self)
+            return storedValue
+        }
+    }
+}
